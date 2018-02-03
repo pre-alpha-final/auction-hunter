63dc0d9bf9ff6e0247aadc705e5f3783f65ea16f
spanishprisoner <jacek.mowinski@gmail.com>
2018-02-03 20:31:37 +0100

    Creating AuctionHunterCore builder

---
+++ src/AuctionHunter/AuctionHunter/Infrastructure/Builders/AuctionHunterCoreBuilder.cs
@@ -0,0 +1,75 @@
+﻿using AuctionHunter.Infrastructure.Implementation;
+using System.Collections.Generic;
+
+namespace AuctionHunter.Infrastructure.Builders
+{
+	public class AuctionHunterCoreBuilder
+	{
+		private string _name;
+		private int _numberOfPages;
+		private string _baseUrl;
+		private IUrlProvider _urlProvider;
+		private IItemsExtractor _itemsExtractor;
+		private IAuctionLinkExtractor _auctionLinkExtractor;
+		private ITitleExtractor _titleExtractor;
+		private List<string> _skipPatterns = new List<string>();
+
+		AuctionHunterCoreBuilder SetName(string name)
+		{
+			_name = name;
+			return this;
+		}
+
+		AuctionHunterCoreBuilder SetNumberOfPages(int numberOfPages)
+		{
+			_numberOfPages = numberOfPages;
+			return this;
+		}
+
+		AuctionHunterCoreBuilder SetUrlProvider(string baseUrl, IUrlProvider urlProvider)
+		{
+			_baseUrl = baseUrl;
+			_urlProvider = urlProvider;
+			return this;
+		}
+
+		AuctionHunterCoreBuilder SetItemsExtractor(IItemsExtractor itemsExtractor)
+		{
+			_itemsExtractor = itemsExtractor;
+			return this;
+		}
+
+		AuctionHunterCoreBuilder SetAuctionLinkExtractor(IAuctionLinkExtractor auctionLinkExtractor)
+		{
+			_auctionLinkExtractor = auctionLinkExtractor;
+			return this;
+		}
+
+		AuctionHunterCoreBuilder SetTitleExtractor(ITitleExtractor titleExtractor)
+		{
+			_titleExtractor = titleExtractor;
+			return this;
+		}
+
+		AuctionHunterCoreBuilder AddSkipPattern(string skipPattern)
+		{
+			_skipPatterns.Add(skipPattern);
+			return this;
+		}
+
+		public IAuctionHunterCore Build()
+		{
+			return new AuctionHunterCore
+			{
+				Name = _name,
+				NumberOfPages = _numberOfPages,
+				BaseUrl = _baseUrl,
+				UrlProvider = _urlProvider,
+				ItemsExtractor = _itemsExtractor,
+				AuctionLinkExtractor = _auctionLinkExtractor,
+				TitleExtractor = _titleExtractor,
+				SkipPatterns = _skipPatterns,
+			};
+		}
+	}
+}
--- src/AuctionHunter/AuctionHunter/Infrastructure/Implementation/AuctionHunterCore.cs
+++ src/AuctionHunter/AuctionHunter/Infrastructure/Implementation/AuctionHunterCore.cs
@@ -1,7 +1,18 @@
-﻿namespace AuctionHunter.Infrastructure.Implementation
+﻿using System.Collections.Generic;
+
+namespace AuctionHunter.Infrastructure.Implementation
 {
 	public class AuctionHunterCore : IAuctionHunterCore
 	{
+		public string Name { get; set; }
+		public int NumberOfPages { get; set; }
+		public string BaseUrl { get; set; }
+		public IUrlProvider UrlProvider { get; set; }
+		public IItemsExtractor ItemsExtractor { get; set; }
+		public IAuctionLinkExtractor AuctionLinkExtractor { get; set; }
+		public ITitleExtractor TitleExtractor { get; set; }
+		public List<string> SkipPatterns { get; set; }
+
 		public void Run()
 		{
 			throw new System.NotImplementedException();
