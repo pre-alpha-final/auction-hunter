0a7602e6f9ec748d8bdb71c48a548dc9e4cc11fc
spanishprisoner <jacek.mowinski@gmail.com>
2018-02-14 22:14:15 +0100

    Basic main page layout

--- src/AuctionHunterFront/Pages/Index.cshtml.cs
+++ src/AuctionHunterFront/Pages/Index.cshtml.cs
@@ -1,14 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using AuctionHunter.Infrastructure;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
 
 namespace AuctionHunterFront.Pages
 {
-    public class IndexModel : PageModel
+	public class IndexModel : PageModel
     {
+		public IList<AuctionItem> DummyData { get; set; } = new List<AuctionItem>
+		{
+			new AuctionItem { AuctionLink = "google.com", OnPage = 1, Content = @"{'prop1', 'value1'}", MarkedAsRead = false, Timestamp = DateTime.Now },
+		};
+
+		public bool HasDummyData => DummyData.Count > 0;
+
         public void OnGet()
         {
 
