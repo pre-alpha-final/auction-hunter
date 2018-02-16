08918284d72e389b3cc5df4325953b75a7a89769
spanishprisoner <jacek.mowinski@gmail.com>
2018-02-16 20:12:53 +0100

    Multi line text on update page

--- src/AuctionHunterFront/Pages/Update/Run.cshtml.cs
+++ src/AuctionHunterFront/Pages/Update/Run.cshtml.cs
@@ -12,9 +12,13 @@ namespace AuctionHunterFront.Pages.Update
 		[BindProperty(SupportsGet = true)]
 		public int? MaxPage { get; set; }
 
+		public string Text { get; set; }
+
 		public async Task<IActionResult> OnGetContinuousPullAsync()
 		{
 			await Task.Delay(100);
+			Text = "Line one\nLine two";
+			Text = Text.Replace("\n", "<br/>");
 			return Page();
 		}
 	}
