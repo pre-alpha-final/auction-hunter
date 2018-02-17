47b635dd941a1052de09847694b3d87b6c6bd8ce
spanishprisoner <jacek.mowinski@gmail.com>
2018-02-17 02:19:59 +0100

    EF and initial migration

---
+++ src/AuctionHunterFront/Models/AuctionHunterContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AuctionHunterFront.Models
+{
+	public class AuctionHunterContext : DbContext
+	{
+		public AuctionHunterContext(DbContextOptions<AuctionHunterContext> options)
+		: base(options)
+		{
+		}
+
+		public DbSet<AuctionHunterItem> AuctionHunterItems { get; set; }
+	}
+}
---
+++ src/AuctionHunterFront/Models/AuctionHunterItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AuctionHunterFront.Models
+{
+	public class AuctionHunterItem
+	{
+		public int ID { get; set; }
+		public string AuctionLink { get; set; }
+		public int OnPage { get; set; }
+		public string AdditionalDataJson { get; set; }
+		public bool MarkedAsRead { get; set; }
+		public DateTime Timestamp { get; set; }
+	}
+}
