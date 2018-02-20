64cc65f692fe15a2733a04f472e5d362f75eb35c
spanishprisoner <jacek.mowinski@gmail.com>
2018-02-20 19:08:51 +0100

    Adding paging

--- src/AuctionHunterFront/Pages/Index.cshtml.cs
+++ src/AuctionHunterFront/Pages/Index.cshtml.cs
@@ -5,6 +5,8 @@ using Microsoft.AspNetCore.Authorization;
 using AuctionHunterFront.Models;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace AuctionHunterFront.Pages
 {
@@ -16,6 +18,13 @@ namespace AuctionHunterFront.Pages
 		public IList<AuctionHunterItem> AuctionHunterItems { get; set; }
 		public bool HasAuctionHunterItems => AuctionHunterItems?.Count > 0;
 
+		public int ItemsPerPage { get; set; } = 12;
+
+		public int ItemCount { get; set; }
+
+		[BindProperty(SupportsGet = true)]
+		public int? PageNumber { get; set; }
+
 		public IndexModel(AuctionHunterDbContext auctionHunterDbContext)
 		{
 			_auctionHunterDbContext = auctionHunterDbContext;
@@ -23,7 +32,12 @@ namespace AuctionHunterFront.Pages
 
 		public async Task OnGetAsync()
 		{
-			AuctionHunterItems = await _auctionHunterDbContext.AuctionHunterItems.ToListAsync();
+			PageNumber = PageNumber ?? 1;
+			ItemCount = await _auctionHunterDbContext.AuctionHunterItems.CountAsync();
+			AuctionHunterItems = await _auctionHunterDbContext.AuctionHunterItems
+				.Skip(ItemsPerPage * ((int)PageNumber - 1))
+				.Take(ItemsPerPage)
+				.ToListAsync();
 		}
 
 		public JToken JsonParse(string json)
