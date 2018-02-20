abefe3e12a439f5dec25c09223bfad4b6af29093
spanishprisoner <jacek.mowinski@gmail.com>
2018-02-20 21:37:40 +0100

    Mark as read functionality

--- src/AuctionHunterFront/Pages/Index.cshtml.cs
+++ src/AuctionHunterFront/Pages/Index.cshtml.cs
@@ -35,9 +35,29 @@ namespace AuctionHunterFront.Pages
 			PageNumber = PageNumber ?? 1;
 			ItemCount = await _auctionHunterDbContext.AuctionHunterItems.CountAsync();
 			AuctionHunterItems = await _auctionHunterDbContext.AuctionHunterItems
+				.Where(e => e.MarkedAsRead == false)
 				.Skip(ItemsPerPage * ((int)PageNumber - 1))
 				.Take(ItemsPerPage)
 				.ToListAsync();
+		}
+
+		public async Task<IActionResult> OnPostMarkAsReadAsync(int id)
+		{
+			var update = new AuctionHunterItem { Id = id };
+			_auctionHunterDbContext.AuctionHunterItems.Attach(update);
+
+			update.MarkedAsRead = true;
+			await _auctionHunterDbContext.SaveChangesAsync();
+
+			return RedirectToPage();
+		}
+
+		public async Task<IActionResult> OnPostMarkAllAsReadAsync()
+		{
+			await _auctionHunterDbContext.Database
+				.ExecuteSqlCommandAsync("UPDATE AuctionHunterItems SET MarkedAsRead = true");
+
+			return RedirectToPage();
 		}
 
 		public JToken JsonParse(string json)
