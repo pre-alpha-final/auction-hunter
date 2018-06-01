6f9af4097a07fffe9696c8c4a7913a4acb9bbf03
spanishprisoner <jacek.mowinski@gmail.com>
2018-06-01 16:58:13 +0200

    Adding origin product id

--- src/AuctionHunterFront/Models/ApplicationUser.cs
+++ src/AuctionHunterFront/Models/ApplicationUser.cs
@@ -5,6 +5,6 @@ namespace AuctionHunterFront.Models
 {
 	public class ApplicationUser : IdentityUser
 	{
-		public Guid OriginProductId { get; set; } = new Guid("1d2f5fda-e58e-43ee-a8a1-88001a6262ad"); // AuctionHunter
+		public Guid OriginProductId { get; set; }
 	}
 }
