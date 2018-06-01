ac2a038a1ca37c31644d54570eb4608c64df7cac
spanishprisoner <jacek.mowinski@gmail.com>
2018-06-01 16:22:03 +0200

    AuctionHunterDbContext changes

--- src/AuctionHunterFront/Migrations/20180601141859_AuctionHunterDbContext Separating identity.Designer.cs
+++ src/AuctionHunterFront/Migrations/20180601141859_AuctionHunterDbContext Separating identity.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace AuctionHunterFront.Migrations
 {
     [DbContext(typeof(AuctionHunterDbContext))]
-    partial class AuctionHunterDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180601141859_AuctionHunterDbContext Separating identity")]
+    partial class AuctionHunterDbContextSeparatingidentity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
