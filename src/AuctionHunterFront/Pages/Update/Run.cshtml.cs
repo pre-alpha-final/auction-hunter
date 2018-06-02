1cb208c1dc446efbe69f3de936c573ccd259617c
spanishprisoner <jacek.mowinski@gmail.com>
2018-06-02 22:33:02 +0200

    Ajax cleanup

--- src/AuctionHunterFront/Pages/Update/Run.cshtml.cs
+++ src/AuctionHunterFront/Pages/Update/Run.cshtml.cs
@@ -1,46 +1,30 @@
-﻿using AuctionHunter.Infrastructure;
-using AuctionHunterFront.Models;
-using AuctionHunterFront.Services;
-using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using System;
-using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
-using AuctionHunterFront.Extensions;
 
 namespace AuctionHunterFront.Pages.Update
 {
 	[Authorize]
 	public class RunModel : PageModel
 	{
-		private readonly AuctionHunterDbContext _auctionHunterDbContext;
-		private readonly IAuctionHunterService _auctionHunterService;
-
 		[BindProperty(SupportsGet = true)]
 		public int? PageNumber { get; set; }
 
-		[BindProperty(SupportsGet = true)]
-		public int? MaxPage { get; set; }
-
 		public string DebugInfo { get; set; }
 
-		public RunModel(AuctionHunterDbContext auctionHunterDbContext, IAuctionHunterService auctionHunterService)
+		public async Task<IActionResult> OnGetAsync()
 		{
-			_auctionHunterDbContext = auctionHunterDbContext;
-			_auctionHunterService = auctionHunterService;
-		}
+			//var pageResult = await _auctionHunterService.GetItems(PageNumber ?? -1);
+			//foreach (var auctionItem in pageResult.AuctionItems.ToList())
+			//{
+			//	await _auctionHunterService.TryAddAsync(_auctionHunterDbContext, auctionItem);
+			//}
+			//await _auctionHunterDbContext.SafeSaveChangesAsync();
+			//DebugInfo = pageResult.DebugInfo.Replace("\n", "<br />");
 
-		public async Task<IActionResult> OnGetContinuousPullAsync()
-		{
-			var pageResult = await _auctionHunterService.GetItems(PageNumber ?? -1);
-			foreach (var auctionItem in pageResult.AuctionItems.ToList())
-			{
-				await _auctionHunterService.TryAddAsync(_auctionHunterDbContext, auctionItem);
-			}
-			await _auctionHunterDbContext.SafeSaveChangesAsync();
-			DebugInfo = pageResult.DebugInfo.Replace("\n", "<br />");
+			await Task.Delay(500);
+			DebugInfo = $"Request for page {PageNumber}<br />Functionality disabled - queries changed to automatic<br />";
 
 			return Page();
 		}
---
+++ src/AuctionHunterFront/wwwroot/js/ajaxhelpers.js
@@ -0,0 +1,31 @@
+﻿async function ajaxGet(url, action) {
+	return new Promise((resolve, reject) => {
+		$.ajax({
+			type: "GET",
+			url: url,
+			success: response => {
+				action(response);
+				resolve(response);
+			}
+		});
+	});
+}
+
+async function ajaxGetAndReplace(url, elementToReplace) {
+	return new Promise((resolve, reject) => {
+		$.ajax({
+			type: "GET",
+			url: url,
+			success: response => {
+				$(elementToReplace).html(response);
+				resolve(response);
+			}
+		});
+	});
+}
+
+async function taskDelay(miliseconds) {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => resolve(), miliseconds);
+	});
+}
