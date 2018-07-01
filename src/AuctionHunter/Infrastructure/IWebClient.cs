a9a89eec70f39c9415af20775f982b7327e16958
spanishprisoner <jacek.mowinski@gmail.com>
2018-07-01 22:39:40 +0200

    Modifying webclient

--- src/AuctionHunter/Infrastructure/IWebClient.cs
+++ src/AuctionHunter/Infrastructure/IWebClient.cs
@@ -1,10 +1,12 @@
 ﻿using AuctionHunter.Results;
+using System.Collections.Specialized;
 using System.Threading.Tasks;
 
 namespace AuctionHunter.Infrastructure
 {
 	public interface IWebClient
 	{
-		Task<WebClientResult> Get(string url);
+		Task<WebClientResult> Get(string url, int retryCount = 5);
+		Task<WebClientResult> Post(string url, NameValueCollection values, int retryCount = 5);
 	}
 }
--- src/AuctionHunter/Infrastructure/Implementation/DefaultWebClient.cs
+++ src/AuctionHunter/Infrastructure/Implementation/DefaultWebClient.cs
@@ -3,26 +3,69 @@ using System;
 using System.Threading.Tasks;
 using AuctionHunter.Controls;
 using AuctionHunter.Extensions;
+using System.Threading;
+using System.Collections.Specialized;
+using System.Text;
 
 namespace AuctionHunter.Infrastructure.Implementation
 {
 	public class DefaultWebClient : IWebClient
 	{
-		private readonly CookieAwareWebClient _webClient = new CookieAwareWebClient();
+		private readonly CookieAwareWebClient _webClient;
+		private readonly SemaphoreSlim _semaphoreSlim = new SemaphoreSlim(1);
 
-		public async Task<WebClientResult> Get(string url)
+		public DefaultWebClient()
+		{
+			_webClient = new CookieAwareWebClient();
+		}
+
+		public async Task<WebClientResult> Get(string url, int retryCount = 5)
+		{
+			Task<string> makeRequest()
+			{
+				_webClient.Headers["User-Agent"] =
+					"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:58.0) Gecko/20100101 Firefox/58.0";
+				var result = _webClient.DownloadString(new Uri(url));
+
+				return Task.FromResult(result);
+			}
+
+			using (await _semaphoreSlim.DisposableWaitAsync(TimeSpan.FromMinutes(10)))
+			{
+				return await RetryableRequest(makeRequest, retryCount);
+			}
+		}
+
+		public async Task<WebClientResult> Post(string url, NameValueCollection values, int retryCount = 5)
+		{
+			Task<string> makeRequest()
+			{
+				_webClient.Headers["User-Agent"] =
+					"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:58.0) Gecko/20100101 Firefox/58.0";
+				_webClient.Headers["Content-Type"] = "application/x-www-form-urlencoded";
+				var result = _webClient.UploadValues(url, "POST", values);
+
+				return Task.FromResult(Encoding.UTF8.GetString(result));
+			}
+
+			using (await _semaphoreSlim.DisposableWaitAsync(TimeSpan.FromMinutes(10)))
+			{
+				return await RetryableRequest(makeRequest, retryCount);
+			}
+		}
+
+		private async Task<WebClientResult> RetryableRequest(Func<Task<string>> makeRequest, int retryCount)
 		{
 			var webClientResult = new WebClientResult();
-
-			for (var i = 0; i < 10; i++)
+			for (var attempt = 0; attempt < retryCount; attempt++)
 			{
 				try
 				{
-					if (i > 0)
+					if (attempt > 0)
 					{
 						webClientResult.DebugInfo.AppendLine("retrying...");
 					}
-					webClientResult.Content = await _webClient.Get(url, true);
+					webClientResult.Content = await makeRequest();
 					webClientResult.DebugInfo.AppendLine("Complete");
 					webClientResult.Success = true;
 					return webClientResult;
@@ -35,7 +78,7 @@ namespace AuctionHunter.Infrastructure.Implementation
 				await Task.Delay(1000);
 			}
 
-			webClientResult.DebugInfo.AppendLine("Download failed");
+			webClientResult.DebugInfo.AppendLine("Failed");
 			return webClientResult;
 		}
 	}
