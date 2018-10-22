dce96845afbf2641ce6425d055fa45f3ddf045eb
spanishprisoner <jacek.mowinski@gmail.com>
2018-10-22 20:26:03 +0200

    Link update

--- src/AuctionHunter/G2A/Implementation/G2ADefaults.cs
+++ src/AuctionHunter/G2A/Implementation/G2ADefaults.cs
@@ -5,7 +5,7 @@ namespace AuctionHunter.G2A.Implementation
 {
 	public static class G2ADefaults
 	{
-		public static string DefaultBaseUrl => "https://www.g2a.com/new/api/products/filter?category_id=games&changeType=PAGINATION&currency=PLN&min_price[max]=100&min_price[min]=0&page=&platform=1&store=polish";
+		public static string DefaultBaseUrl => "https://www.g2a.com/new/api/v1/products/filter?category_id=games&changeType=PAGINATION&currency=PLN&min_price[max]=100&min_price[min]=0&page=&platform=1&store=polish";
 		public static IG2AUrlProvider DefaultUrlProvider => Program.Container.GetService<IG2AUrlProvider>();
 		public static IWebClient DefaultWebCllient => Program.Container.GetService<IWebClient>();
 		public static IG2AItemsExtractor DefaultItemsExtractor => Program.Container.GetService<IG2AItemsExtractor>();
